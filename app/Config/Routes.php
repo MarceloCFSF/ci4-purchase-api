83eb15a6f70ea04ce1fb10ea15d1fcbd5efc3ae9
MarceloCFSF <marcelo.cfsf@gmail.com>
2025-03-03 14:38:12 -0300

    feat: remove unused route home

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,7 +5,6 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
 $routes->get('health', 'HealthCheck::db');
 $routes->resource('clients', ['controller' => 'ClientController']);
 $routes->resource('products', ['controller' => 'ProductController']);
